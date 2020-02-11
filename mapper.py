c3af27565ac0cef2a22fd095800212cc45c1f94a
Rajesh <rajeshramesh38@gmail.com>
2020-02-11 20:12:04 +0530

    Phase 01

--- mapper.py
+++ mapper.py
@@ -1,17 +1,27 @@
-password = input("user password?")
-mapper_matrix = [['C', 'D', 'E', 'F'], ['8', '9', 'A', 'B'], ['4', '5', '6', '7'], ['0', '1', '2', '3']]
-patterns = {'A': [(3, 0), (2, 0), (1, 0), (0, 1), (0, 2), (1, 3), (2, 3), (3, 3), (2, 0), (2, 1), (2, 2), (2, 3)]}
+import json
+
+mapper_matrix = [['C', 'D', 'E', 'F'],
+                 ['8', '9', 'A', 'B'],
+                 ['4', '5', '6', '7'],
+                 ['0', '1', '2', '3']]
 
 
-def mapping(password):
+def mapping(password, patterns):
     temp = password.upper()
+    encoded = []
     for letter in temp:
         mappings = patterns[letter]
         secret = ""
         for map in mappings:
             secret += mapper_matrix[map[0]][map[1]]
-    return secret
+        encoded.append((letter, secret))
+    return encoded
 
 
-split = range(2)
-mapped = mapping(password)
+passwd = input("user password?")
+
+d = open("dataset.json", "r")
+data = json.load(d)
+patterns = data
+mapped = mapping(passwd, patterns)
+print(mapped)
